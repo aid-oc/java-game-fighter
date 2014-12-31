5e4c07e3315fe9396e2485c9f0cafe48a553d8e7
Ozzy <Ozzy@d38b9b40-c97a-4d20-ac8e-4ddea1c28b56>
2014-12-31 04:07:28 +0000

    Added debug to show location of fxml file
    git-svn-id: https://svn.runemate.com/svn/ozzy@32 d38b9b40-c97a-4d20-ac8e-4ddea1c28b56

--- src/scripts/MassFighter/GUI/Main.java
+++ src/scripts/MassFighter/GUI/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 public class Main extends Application {
 
     private Controller controller;
@@ -14,6 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("FighterV1.fxml"));
+        JOptionPane.showMessageDialog(null, "Found in location: " + getClass().getResource("FighterV1.fxml"), "MassFighter", JOptionPane.WARNING_MESSAGE);
         Parent root = loader.load();
         primaryStage.setTitle("MassFighter V4");
         primaryStage.setScene(new Scene(root));
