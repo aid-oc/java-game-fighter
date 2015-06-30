c1627bcea6d9207ad3241b87d60caba58bf04ecb
Ozzy <Ozzy@d38b9b40-c97a-4d20-ac8e-4ddea1c28b56>
2015-06-30 14:28:28 +0000

    Fix for arrow re-equipping
    git-svn-id: https://svn.runemate.com/svn/ozzy@100 d38b9b40-c97a-4d20-ac8e-4ddea1c28b56

--- src/scripts/MassFighter/Tasks/Ammunition.java
+++ src/scripts/MassFighter/Tasks/Ammunition.java
@@ -1,64 +1,64 @@
 package scripts.MassFighter.Tasks;
 
+import com.runemate.game.api.hybrid.entities.definitions.ItemDefinition;
 import com.runemate.game.api.hybrid.local.hud.interfaces.Equipment;
 import com.runemate.game.api.hybrid.local.hud.interfaces.Inventory;
 import com.runemate.game.api.hybrid.local.hud.interfaces.SpriteItem;
 import com.runemate.game.api.hybrid.queries.SpriteItemQueryBuilder;
-import com.runemate.game.api.hybrid.queries.results.SpriteItemQueryResults;
 import com.runemate.game.api.hybrid.util.Filter;
+import com.runemate.game.api.hybrid.util.Filters;
 import com.runemate.game.api.script.Execution;
 import com.runemate.game.api.script.framework.task.Task;
-import scripts.MassFighter.MassFighter;
 
-import static scripts.MassFighter.Framework.Methods.*;
 
 public class Ammunition extends Task {
 
-    private final SpriteItemQueryBuilder playerHasEquippedAmmunition = Equipment.newQuery().filter(new Filter<SpriteItem>() {
+    private final SpriteItemQueryBuilder availableAmmunition = Inventory.newQuery().filter(new Filter<SpriteItem>() {
         @Override
         public boolean accepts(SpriteItem spriteItem) {
-            String spriteItemName = spriteItem.getDefinition().getName().toLowerCase();
-            return spriteItemName.contains("arrow") || spriteItemName.contains("bolt");
+            String itemName = null;
+            if (spriteItem != null) {
+                ItemDefinition itemDefinition = spriteItem.getDefinition();
+                if (itemDefinition != null) itemName = itemDefinition.getName();
+            }
+            return itemName != null && (itemName.contains("arrow") || itemName.contains("bolt"));
         }
     });
 
-    private final SpriteItemQueryBuilder inventoryContainsAmmunition = Inventory.newQuery().filter(new Filter<SpriteItem>() {
-        @Override
-        public boolean accepts(SpriteItem spriteItem) {
-            String spriteItemName = spriteItem.getDefinition().getName().toLowerCase();
-            return spriteItemName.contains("arrow") || spriteItemName.contains("bolt");
+    private SpriteItemQueryBuilder getMatchingAmmunition() {
+        SpriteItemQueryBuilder matchingAmmunition = Inventory.newQuery().filter(Filters.DECLINE_ALL);
+        if (getCurrentAmmunition() != null && getCurrentAmmunition().getDefinition() != null) {
+            String currentAmmoName = getCurrentAmmunition().getDefinition().getName();
+            if (currentAmmoName != null) {
+                matchingAmmunition = availableAmmunition.filter(item -> item != null && item.getDefinition() != null && item.getDefinition().getName().equals(currentAmmoName));
+            }
         }
-    });
+        return matchingAmmunition;
+    }
+
+    private SpriteItem getCurrentAmmunition() {
+        return Equipment.getItemIn(Equipment.Slot.AMMUNITION);
+    }
+
+    private boolean shouldEquip() {
+        return (getCurrentAmmunition() != null && !getMatchingAmmunition().results().isEmpty()) || getCurrentAmmunition() == null;
+    }
 
 
     public boolean validate() {
-        return !playerHasEquippedAmmunition.results().isEmpty() && inventoryContainsAmmunition.results().isEmpty();
+        return !availableAmmunition.results().isEmpty() && shouldEquip();
     }
 
     @Override
     public void execute() {
-        SpriteItem equippedAmmunition = playerHasEquippedAmmunition.results().first();
-        if (equippedAmmunition != null) {
-            String equippedAmmunitionName = equippedAmmunition.getDefinition().getName();
-            SpriteItemQueryResults inventoryAmmunitionResults = inventoryContainsAmmunition.filter(new Filter<SpriteItem>() {
-                @Override
-                public boolean accepts(SpriteItem spriteItem) {
-                    return spriteItem.getDefinition().getName().equals(equippedAmmunitionName);
-                }
-            }).results();
-            if (!inventoryAmmunitionResults.isEmpty()) {
-                MassFighter.status = "Equipping Arrows";
-                out("Ammunition: Found a valid type");
-                SpriteItem targetAmmunition = inventoryAmmunitionResults.first();
-                if (targetAmmunition != null && targetAmmunition.interact("Wield")) {
-                    out("Ammunition: Equipped the item");
-                    Execution.delayUntil(() -> !Inventory.contains(targetAmmunition.getId()), 1500, 2000);
-                }
-            } else {
-                out("Ammunition: This item is not what we have equipped");
-            }
+        SpriteItem targetAmmo;
+        if (getCurrentAmmunition() != null) {
+            targetAmmo = getMatchingAmmunition().results().random();
         } else {
-            out("Ammunition: Can't get info on what we have equipped");
+            targetAmmo = availableAmmunition.results().random();
+        }
+        if (targetAmmo != null && targetAmmo.interact("Wield")) {
+            Execution.delayUntil(() -> !targetAmmo.isValid(), 2000);
         }
     }
 
